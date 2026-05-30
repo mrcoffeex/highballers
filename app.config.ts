e2318080b4840196018a6a097c7e367f43baef33
Kent John Go <kjohn0319@gmail.com>
2026-05-30 16:15:27 +0800

    Refactor app configuration to utilize ConfigContext for ExpoConfig, update owner name in app.json, and add expo-font dependency. Adjust EAS CLI version in eas.json and streamline build scripts in package.json for improved compatibility.

--- app.config.ts
+++ app.config.ts
@@ -1,14 +1,10 @@
-import type { ExpoConfig } from "expo/config";
+import type { ConfigContext, ExpoConfig } from "expo/config";
 
-import appJson from "./app.json";
-
-const expo = appJson.expo as ExpoConfig;
-
-export default (): ExpoConfig => ({
-  ...expo,
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
   owner: "kentjohngo",
   extra: {
-    ...expo.extra,
+    ...config.extra,
     eas: {
       projectId: "d8bdb9a0-8b35-4786-b51f-c5b57f808720",
     },
